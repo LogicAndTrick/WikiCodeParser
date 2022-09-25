25e32882c717ee62558d0c35606d82a73b30fd09
Daniel Walder <daniel.walder@gmail.com>
2022-09-25 10:51:09 +1000

    Add unit test project

---
+++ LogicAndTrick.WikiCodeParser.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace LogicAndTrick.WikiCodeParser.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
