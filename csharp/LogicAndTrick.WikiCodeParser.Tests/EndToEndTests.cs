7922bfb37905f28d1df0df3b22b4edc8ae8ea0c4
Daniel Walder <daniel.walder@gmail.com>
2022-10-27 23:53:09 +1000

    Implement isolated/end to end test cases in .net

---
+++ csharp/LogicAndTrick.WikiCodeParser.Tests/EndToEndTests.cs
@@ -0,0 +1,12 @@
+﻿namespace LogicAndTrick.WikiCodeParser.Tests;
+
+[TestClass]
+public class EndToEndTests
+{
+    [DataTestMethod]
+    [DataRow("wikicode-page")]
+    public void EndToEndTest(string name)
+    {
+        TestCaseUtils.RunTestCase("endtoend", name);
+    }
+}
---
+++ csharp/LogicAndTrick.WikiCodeParser.Tests/TestCaseUtils.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LogicAndTrick.WikiCodeParser.Tests;
+
+public static class TestCaseUtils
+{
+    private static void Test(string input, string expectedOutput, string? expectedMeta, bool split = false)
+    {
+        var config = ParserConfiguration.Default();
+        var parser = new Parser(config);
+
+        var result = parser.ParseResult(input);
+        var resultHtml = result.ToHtml().Trim();
+
+        if (split)
+        {
+            var expectedLines = expectedOutput.Split('\n');
+            var actualLines = resultHtml.Split('\n');
+
+            for (var i = 0; i < expectedLines.Length; i++)
+            {
+                var ex = expectedLines[i];
+                var ac = actualLines[i];
+                Assert.AreEqual(ex, ac, $"\n\nMatch failed on line {i + 1}.\nExpected: {ex}\nActual  : {ac}");
+            }
+        }
+        else
+        {
+            Assert.AreEqual(expectedOutput, resultHtml);
+        }
+
+        if (expectedMeta != null)
+        {
+            var resultMetaObjects = result.GetMetadata().Select(x => new { x.Key, Value = JsonConvert.DeserializeObject(JsonConvert.SerializeObject(x.Value)) }).ToList();
+            var expectedMetaObjects = expectedMeta.Split('\n').Select(x => x.Trim().Split('=', 2)).Select(x => new { Key = x[0], Value = JsonConvert.DeserializeObject(x[1]) }).ToList();
+            Assert.AreEqual(expectedMetaObjects.Count, resultMetaObjects.Count);
+            for (var i = 0; i < resultMetaObjects.Count; i++)
+            {
+                var rmo = resultMetaObjects[i];
+                var emo = expectedMetaObjects[i];
+                if (emo.Value.ToString() == rmo.Value.ToString()) continue;
+                var robj = (JObject)rmo.Value;
+                var eobj = (JObject)rmo.Value;
+                foreach (var ep in eobj.Properties())
+                {
+                    var rp = robj.Property(ep.Name);
+                    var ev = ep.Value?.ToString();
+                    var rv = rp.Value?.ToString();
+                    Assert.AreEqual(ev, rv);
+                }
+            }
+        }
+    }
+
+    public static string GetTestCaseDirectory(string folder)
+    {
+        if (Environment.GetEnvironmentVariable("VSTEST_BACKGROUND_DISCOVERY") == "1")
+        {
+            // Live unit testing
+            // I don't know a better way, so for now we hard-code
+            return Path.Combine(@"D:\Github\WikiCodeParser\tests", folder);
+        }
+
+        // Walk up the folder tree until we hit the repo root
+        var dir = Environment.CurrentDirectory;
+        while (dir != null && !Directory.Exists(Path.Combine(dir, "tests", folder)))
+        {
+            dir = Path.GetDirectoryName(dir);
+        }
+
+        if (dir == null) throw new FileNotFoundException("Cannot find test case folder", folder);
+
+        return Path.Combine(dir, "tests", folder);
+    }
+
+    // ReSharper disable InconsistentNaming
+    public static void RunTestCase(string folder, string name, bool split = false)
+    {
+        var dir = GetTestCaseDirectory(folder);
+        string _in;
+        string _out;
+        string? _meta = null;
+        if (File.Exists($"{dir}/{name}"))
+        {
+            var text = File.ReadAllText($"{dir}/{name}");
+            var spl = text.Split("###").Select(x => x.Trim()).ToArray();
+            _in = spl[0];
+            _out = spl[1];
+            if (spl.Length > 2) _meta = spl[2];
+        }
+        else
+        {
+            _in = File.ReadAllText($"{dir}/{name}.in");
+            _out = File.ReadAllText($"{dir}/{name}.out");
+            if (File.Exists($"{dir}/{name}.meta"))
+            {
+                _meta = File.ReadAllText($"{dir}/{name}.meta");
+            }
+        }
+
+        _in = _in.Replace("\r", "");
+        _out = _out.Replace("\r", "");
+        _meta = _meta?.Replace("\r", "");
+        Test(_in, _out, _meta, split);
+    }
+    // ReSharper restore InconsistentNaming
+}
