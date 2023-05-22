c9c206754745771b9c04f8bf4baf6f567189db41
Daniel Walder <daniel.walder@gmail.com>
2023-05-22 17:14:34 +1000

    Fix issue in javascript WikiRevision CreateSlug method

--- javascript/src/Models/WikiRevision.ts
+++ javascript/src/Models/WikiRevision.ts
@@ -1,7 +1,7 @@
 
 export class WikiRevision {
     public static CreateSlug(text : string) {
-        text = text.replace(' ', '_');
+        text = text.replace(/ /ig, '_');
         text = text.replace(/[^-$_.+!*'"(),:;<>^{}|~0-9a-z[\]]/ig, '');
         return text;
     }
